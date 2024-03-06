f1320fdf765c9f7375caf047cf33ccdf78bcc2ea
Catalina Bedoya <78061240+CatalinaBedoya1@users.noreply.github.com>
2024-03-06 10:23:41 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="leftSide"></div>
+        <div className="rightSide"></div>
+        
+      
+    </div>
+  )
+}
+
+export default Navbar
+
+
+// use rfce
