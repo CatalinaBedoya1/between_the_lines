318a0492bf7d3e0e6d5cb18373f6e1341f291103
Catalina Bedoya <78061240+CatalinaBedoya1@users.noreply.github.com>
2024-03-10 15:14:10 -0400

    footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import './Footer.css';
+import { useCallback } from "react";
+import "./Footer.css";
 
-function Footer() {
-  const currentYear = new Date().getFullYear();
+const Footer = () => {
+  const onAboutUsTermsClick = useCallback(() => {
+    // Please sync "about us page" to the project
+  }, []);
 
   return (
     <footer className="footer">
-        <div className="container">
-            <p>This is the footer of our website</p>
-            <p>About Us</p>
+      <div className="footer1" />
+      <div className="about-us-terms-container" onClick={onAboutUsTermsClick}>
+        <p className="about-us">{`About Us `}</p>
+        <p className="blank-line4">&nbsp;</p>
+        <p className="blank-line5">&nbsp;</p>
+        <p className="terms-of-use">Terms of Use           Privacy Policy</p>
+        <p className="blank-line6">&nbsp;</p>
+      </div>
+      <div className="copyright-icon-frame1">
+        <div className="about-us-frame">
+          <div className="footer-info">
+            <img
+              className="copyright-icon"
+              loading="lazy"
+              alt=""
+              src="/copyright-icon.svg"
+            />
+          </div>
+          <div className="copyright-icon1">
+            <div className="between-the-lines-container">
+              <p className="between-the-lines1">2024 Between the Lines</p>
+              <p className="blank-line7">&nbsp;</p>
+            </div>
+          </div>
         </div>
-        <div className="copyright">
-            <p>&copy; {currentYear} Between The Lines</p>
-        </div>
+      </div>
     </footer>
   );
-}
+};
 
 export default Footer;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 
+
 // import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className="home">
+      <div className="hero-section">
         <HeroSection />
+      </div>
     </div>
   );
 }
