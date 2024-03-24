ad756f5d856c45666f2d00066ffd2fcb457f6a92
Justine Wehrli <justin223127085@gmail.com>
2024-03-24 05:22:45 -0400

    About us section

--- src/routes/AboutUs.js
+++ src/routes/AboutUs.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import AboutUsHero from '../components/AboutUsHero';
+import AboutUsBody from '../components/AboutUsBody';
+import './AboutUs.css';
 
 function AboutUs() {
     return (
         <div className="aboutus">
-            <h1>About Us</h1>
-        </div>
+        <AboutUsHero />
+        <AboutUsBody />
+
+      </div>  
     );
 }
 
