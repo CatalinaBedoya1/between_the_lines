f3c0f0858ce03283abf23462f6f4fef822049532
Justine Wehrli <justin223127085@gmail.com>
2024-03-26 23:27:27 -0400

    About us page fixes

--- src/components/AboutUsHero.js
+++ src/components/AboutUsHero.js
@@ -5,14 +5,14 @@ import './AboutUsHero.css';
 
 function AboutUsHero() {
     return (
-        <div class="container">       
-        <div class="text">
+        <div class="aboutus-container">       
+        <div class="aboutus-text">
         <h1>About Us</h1>
         <br></br>
           <h3>We are a team of avid readers hoping to create a safe space to join communities, find recommendations, and meet other book enthusiasts. </h3>
         </div>
         <div class="image">
-          <img src={AboutUsImage} alt="Hero section image" className="hero-image" />
+          <img src={AboutUsImage} alt="Hero section art" className="aboutus-hero-image" />
         </div>
       </div>
 );
