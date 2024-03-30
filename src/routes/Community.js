a7a9be570dd7e6e6a47f661c10f477f06ae5c7fe
Reya-das <reya.das79@myhunter.cuny.edu>
2024-03-29 22:16:44 -0400

    community header

--- src/routes/Community.js
+++ src/routes/Community.js
@@ -7,7 +7,13 @@ import Leaderboard from '../assets/Leaderboard.png';
 export const Community = () => {
     return (
       <div className='community'>
-        <img src={herosectionimage} alt="HeroSection" />
+        <div classname= 'communityheader'>
+          <img src={herosectionimage} alt="HeroSection" />
+          <div className='hero-community-content'>
+            <h1>Welcome Jane Doe,</h1>
+            <h1>Let's see what's new in the Community ...</h1>
+          </div>
+          </div>
         <Events />        
     
         <div className="leaderboard-container">
