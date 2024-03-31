3b4e2ebdfa9b4b3c2ed59632ec4d640ca26f98f5
Francesca Tan <erica@francescasmbp3.mynetworksettings.com>
2024-03-30 22:01:00 -0400

    Dashboard bio

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './Dashboard.css';
 import DashboardRecs from "../components/DashboardRecs";
 import QuizRecs from "../components/QuizResults";
+import DashIcon from "../assets/dashboardicon.png";
 
 
 
 function Dashboard() {
     return (
         <div className="dashboard">
-            <h2>Dashboard</h2>
+            <img src={DashIcon} alt="DashboardIcon" className="Dashboard-Icon" />
+            <div className = "bio">
+                <h1> Jane Doe</h1>
+                <h2>Joined March 2024</h2>
+                <p> 24 | Constantly lost in fictional worlds | Avid Romance Reader </p>
+            </div>
             <div className = "topgenres">
                 <p>My Top Genres:</p>
                 <div className = "topgenre1">Fiction</div>
