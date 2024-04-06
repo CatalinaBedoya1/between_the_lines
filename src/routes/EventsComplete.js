0a24dd2963870c1b5105ca23596a6478942c5fb8
Francesca Tan <erica@francescasmbp3.mynetworksettings.com>
2024-04-05 22:05:12 -0400

    eventscomplete

--- src/routes/EventsComplete.js
+++ src/routes/EventsComplete.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import EventTicket from '../assets/eventticket.png';
+import './EventsComplete.css';
 
 function EventsComplete() {
     return(
@@ -7,12 +8,16 @@ function EventsComplete() {
             <div className='thankyoutext'>
                 <h1>Thank you for registering for the</h1>
                 <h2>Webinar with Stephan King</h2>
+                <h3>Please find your ticket below</h3>
             </div>
             <img src={EventTicket} alt="Event Ticket" className="EventTicket" />
-            <div className='eventscompletebtns'></div>
-                <div className = "downloadticket">Download Ticket</div>
-                <div className = "shareticket">Share Ticket</div>
-                <div className = "donebtn">Done</div>
+            <div className='Eventbtnsrow1'>
+            <div className = "downloadticket">Download Ticket</div>
+            <div className = "shareticket">Share Ticket</div>
+            </div>
+            <div className='Eventbtnsrow2'>
+            <div className = "donebtn">Done</div>
+            </div>
         </div>
         
     );
