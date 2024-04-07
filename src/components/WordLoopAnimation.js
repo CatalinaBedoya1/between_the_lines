1c777ac25aa779721fdc7c4cfb92925f69f744f2
Catalina Bedoya <78061240+CatalinaBedoya1@users.noreply.github.com>
2024-04-07 12:22:01 -0400

    typing animation

--- src/components/WordLoopAnimation.js
+++ src/components/WordLoopAnimation.js
@@ -6,28 +6,70 @@ const WordDiscussionTitle = styled.h1`
   font-family: Roboto;
   color: white;
   align-items: center;
+  position: relative; 
 `;
 
-const Discussions = () => {
+const Cursor = styled.span`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 10px;
+  background-color: white;
+  animation: blink 0.8s infinite; /* Blink animation */
+  @keyframes blink {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`;
+
+const WordLoopAnimation = () => {
   const words = ['Groups', 'Author', 'Title', 'Genre', 'Topics'];
   const [displayedWord, setDisplayedWord] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [charIndex, setCharIndex] = useState(0);
+  const [isTyping, setIsTyping] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      const nextIndex = (currentIndex + 1) % words.length;
-      setDisplayedWord(words[nextIndex]);
-      setCurrentIndex(nextIndex);
-    }, 1000); // Adjust the delay between each word
+      if (isTyping) {
+        if (charIndex < words[currentIndex].length) {
+          setDisplayedWord(prevWord => prevWord + words[currentIndex][charIndex]);
+          setCharIndex(prevIndex => prevIndex + 1);
+        } else {
+          setIsTyping(false);
+          setTimeout(() => setIsTyping(true), 1000); 
+        }
+      } else {
+        if (displayedWord.length > 0) {
+          setDisplayedWord(prevWord => prevWord.slice(0, -1));
+        } else {
+          setCurrentIndex(prevIndex => (prevIndex + 1) % words.length);
+          setCharIndex(0);
+        }
+      }
+    }, 150); // Typing speed
 
     return () => clearTimeout(timer);
-  }, [currentIndex, words]);
+  }, [charIndex, currentIndex, displayedWord, isTyping, words]);
 
   return (
     <WordDiscussionTitle>
       {displayedWord}
+      {isTyping && <Cursor />} 
     </WordDiscussionTitle>
   );
 };
 
-export default Discussions;
+export default WordLoopAnimation;
+
+
+
+
