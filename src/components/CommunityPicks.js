5f0f125d4ccbec3f90323c7d59ee13b469b74c71
MadelineLoo <madwasnthere@gmail.com>
2024-04-07 21:17:19 -0400

    dashboard book sizing changed

--- src/components/CommunityPicks.js
+++ src/components/CommunityPicks.js
@@ -1,153 +1,13 @@
-// ORIGINAL CODE
-import React, { useRef, useState, useEffect } from 'react';
-import seeMore from "../assets/seemorebtn.png";
-import styled from 'styled-components';
-
-const NYT_API_KEY = 've27qt7otDqwAHzuCuLsr9M3inbBinNe';
-
-const NewCPPicks = () => {
-    return (
-        <NewContainer>
-            <NewSection category="hardcover-fiction" />
-            <NewSection category="hardcover-nonfiction" />
-            <NewSection category="trade-fiction-paperback" />
-            <NewSection category="young-adult-hardcover" />
-
-            <div className="seemorebtn">
-                <img src={seeMore} alt="seemore" className="seemore-static" />
-            </div>
-        </NewContainer>
-    );
-};
-
-const NewSection = ({ category }) => {
-    const newContainerRef = useRef();
-    const [books, setBooks] = useState([]);
-    const [title, setTitle] = useState("");
-
-    useEffect(() => {
-        const fetchBooks = async () => {
-            try {
-                const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${category}.json?api-key=${NYT_API_KEY}`);
-                const data = await response.json();
-                setBooks(data.results.books || []);
-                setTitle(mapTitle(category)); // Mapping function to get custom title
-            } catch (error) {
-                console.error(`Error fetching ${category} books:`, error);
-            }
-        };
-
-        fetchBooks();
-    }, [category]);
-
-
-    // Mapping function to get custom title
-    const mapTitle = (category) => {
-        switch (category) {
-            case "hardcover-fiction":
-                return "Top Community Picks Fiction";
-            case "hardcover-nonfiction":
-                return "Top Community Picks in Nonfiction";
-            case "trade-fiction-paperback":
-                return "Top Community Picks in Young Adult Fiction";
-            case "young-adult-hardcover":
-                return "Top Community Picks in Fantasy ";
-            default:
-                return "";
-        }
-    };
-
-    return (
-        <>
-            <NewQuizResult>
-                <p>{title}</p> {/* Display custom title */}
-            </NewQuizResult>
-
-            <div
-                ref={newContainerRef}
-                style={{
-                    width: "1150px",
-                    overflowX: "scroll",
-                    scrollBehavior: "smooth",
-                }}
-            >
-                <NewContentBox>
-                    {books.map((book, index) => (
-                        <NewCPcard key={index}>
-                            <BookCover src={book.book_image} alt={book.title} />
-                            <BookTitle>{book.title}</BookTitle>
-                        </NewCPcard>
-                    ))}
-                </NewContentBox>
-            </div>
-
-
-        </>
-    );
-};
-
-export default NewCPPicks;
-
-const NewQuizResult = styled.div`
-    margin-bottom: 20px;
-    text-align: left;
-    margin-left: 130px;
-    width: 100%;
-`;
-
-const NewContainer = styled.div`
-    width: 100vw;
-    height: 300vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`;
-
-const NewContentBox = styled.div`
-    width: 1800px;
-    display: flex;
-    align-items: center;
-    gap: 10px;
-`;
-
-const NewCPcard = styled.div`
-    width: 214px;
-    height: 314px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background-color: ${(props) => props.color};
-`;
-
-const BookCover = styled.img`
-    width: 150px;
-    height: 200px;
-`;
-
-const BookTitle = styled.p`
-    font-size: 16px;
-    margin-top: 10px;
-`;
-
-
-
-
-
-
-// EDITED CODE WIP- MADDY
-
+// // ORIGINAL CODE
 // import React, { useRef, useState, useEffect } from 'react';
 // import seeMore from "../assets/seemorebtn.png";
 // import styled from 'styled-components';
 
-
-// const NYT_API_KEY = 'EhyMWDy9K5iT2H5QRhcpOrSaBGlBtm1G';
+// const NYT_API_KEY = 've27qt7otDqwAHzuCuLsr9M3inbBinNe';
 
 // const NewCPPicks = () => {
 //     return (
-//         <Container>
+//         <NewContainer>
 //             <NewSection category="hardcover-fiction" />
 //             <NewSection category="hardcover-nonfiction" />
 //             <NewSection category="trade-fiction-paperback" />
@@ -156,12 +16,12 @@ const BookTitle = styled.p`
 //             <div className="seemorebtn">
 //                 <img src={seeMore} alt="seemore" className="seemore-static" />
 //             </div>
-//         </Container>
+//         </NewContainer>
 //     );
 // };
 
 // const NewSection = ({ category }) => {
-//     const containerRef = useRef();
+//     const newContainerRef = useRef();
 //     const [books, setBooks] = useState([]);
 //     const [title, setTitle] = useState("");
 
@@ -203,111 +63,251 @@ const BookTitle = styled.p`
 //                 <p>{title}</p> {/* Display custom title */}
 //             </NewQuizResult>
 
-
-//             <ScrollContainer ref={containerRef}>
-//                 <ContentBox>
+//             <div
+//                 ref={newContainerRef}
+//                 style={{
+//                     width: "1150px",
+//                     overflowX: "scroll",
+//                     scrollBehavior: "smooth",
+//                 }}
+//             >
+//                 <NewContentBox>
 //                     {books.map((book, index) => (
-//                         <CPcard key={index}>
+//                         <NewCPcard key={index}>
 //                             <BookCover src={book.book_image} alt={book.title} />
-//                             {/* <BookTitle>{book.title}</BookTitle> */}
-//                         </CPcard>
+//                             <BookTitle>{book.title}</BookTitle>
+//                         </NewCPcard>
 //                     ))}
-//                 </ContentBox>
-//             </ScrollContainer>
-            
+//                 </NewContentBox>
+//             </div>
+
+
 //         </>
 //     );
 // };
 
 // export default NewCPPicks;
 
-
-
-// /*style containers*/
-
 // const NewQuizResult = styled.div`
-//     margin-bottom: 30px;
+//     margin-bottom: 20px;
 //     text-align: left;
 //     margin-left: 130px;
 //     width: 100%;
-//     //padding: 20px;
 // `;
 
-// const Container = styled.div`  //whole container
+// const NewContainer = styled.div`
 //     width: 100vw;
-//     height: 225vh;
+//     height: 300vh;
 //     display: flex;
 //     flex-direction: column;
 //     align-items: center;
 //     justify-content: center;
-//     //background-color: red;
-//     //padding-top: 50px;
 // `;
 
-// const ScrollContainer = styled.div`
-//     width: 1150px;
-//     height: 500px; //moves scroll bar
-//     overflow-x: scroll;
-//     scroll-behavior: smooth;
-//     //background-color: green;
-//     padding-top: 10px;  //spacing on top of books imgs
-
-//     &::-webkit-scrollbar {
-//         width: 10px;
-//     }
-
-//     /* Track */
-//     &::-webkit-scrollbar-track {
-//         background: #d6d6d6;
-//         border-radius: 10px;
-//     }
-
-//     /* Handle */
-//     &::-webkit-scrollbar-thumb {
-//         background: #5397AC;
-//         border-radius: 10px;
-//     }
-
-//     /* Handle on hover */
-//     &::-webkit-scrollbar-thumb:hover {
-//         background: #555;
-//     }
-// `;
-
-// const ContentBox = styled.div`
-//     width: 2250px;
+// const NewContentBox = styled.div`
+//     width: 1800px;
 //     display: flex;
 //     align-items: center;
-//     gap: 15px;          //spacing between cards
-//     //background-color: blue;
+//     gap: 10px;
 // `;
 
-// const CPcard = styled.div`
-//     width: 150px;
-//     height: 200px;
+// const NewCPcard = styled.div`
+//     width: 214px;
+//     height: 314px;
 //     display: flex;
 //     flex-direction: column;
 //     align-items: center;
 //     justify-content: center;
 //     background-color: ${(props) => props.color};
-
-//     &:hover {
-//         transform: scale(1.1); /* Increase the size of the card on hover */
-//         transition: transform 0.3s ease; /* Add a smooth transition effect */
-//     }
 // `;
 
 // const BookCover = styled.img`
 //     width: 150px;
 //     height: 200px;
-
-//     /* hover effect */
-//     ${CPcard}:hover & {
-//         filter: brightness(70%); 
-//     }
 // `;
 
 // const BookTitle = styled.p`
 //     font-size: 16px;
 //     margin-top: 10px;
 // `;
+
+
+
+
+
+
+// EDITED CODE WIP- MADDY
+
+import React, { useRef, useState, useEffect } from 'react';
+import seeMore from "../assets/seemorebtn.png";
+import styled from 'styled-components';
+
+
+const NYT_API_KEY = 've27qt7otDqwAHzuCuLsr9M3inbBinNe';
+
+const NewCPPicks = () => {
+    return (
+        <Container>
+            <NewSection category="hardcover-fiction" />
+            <NewSection category="hardcover-nonfiction" />
+            <NewSection category="trade-fiction-paperback" />
+            <NewSection category="young-adult-hardcover" />
+
+            <div className="seemorebtn">
+                <img src={seeMore} alt="seemore" className="seemore-static" />
+            </div>
+        </Container>
+    );
+};
+
+const NewSection = ({ category }) => {
+    const containerRef = useRef();
+    const [books, setBooks] = useState([]);
+    const [title, setTitle] = useState("");
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            try {
+                const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${category}.json?api-key=${NYT_API_KEY}`);
+                const data = await response.json();
+                setBooks(data.results.books || []);
+                setTitle(mapTitle(category)); // Mapping function to get custom title
+            } catch (error) {
+                console.error(`Error fetching ${category} books:`, error);
+            }
+        };
+
+        fetchBooks();
+    }, [category]);
+
+
+    // Mapping function to get custom title
+    const mapTitle = (category) => {
+        switch (category) {
+            case "hardcover-fiction":
+                return "Top Community Picks Fiction";
+            case "hardcover-nonfiction":
+                return "Top Community Picks in Nonfiction";
+            case "trade-fiction-paperback":
+                return "Top Community Picks in Young Adult Fiction";
+            case "young-adult-hardcover":
+                return "Top Community Picks in Fantasy ";
+            default:
+                return "";
+        }
+    };
+
+    return (
+        <>
+            <NewQuizResult>
+                <p>{title}</p> {/* Display custom title */}
+            </NewQuizResult>
+
+
+            <ScrollContainer ref={containerRef}>
+                <ContentBox>
+                    {books.map((book, index) => (
+                        <CPcard key={index}>
+                            <BookCover src={book.book_image} alt={book.title} />
+                            {/* <BookTitle>{book.title}</BookTitle> */}
+                        </CPcard>
+                    ))}
+                </ContentBox>
+            </ScrollContainer>
+            
+        </>
+    );
+};
+
+export default NewCPPicks;
+
+
+
+/*style containers*/
+
+const NewQuizResult = styled.div`
+    margin-bottom: 30px;
+    text-align: left;
+    margin-left: 130px;
+    width: 100%;
+    //padding: 20px;
+`;
+
+const Container = styled.div`  //whole container
+    width: 100vw;
+    height: 225vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    //background-color: red;
+    //padding-top: 50px;
+`;
+
+const ScrollContainer = styled.div`
+    width: 1150px;
+    height: 500px; //moves scroll bar
+    overflow-x: scroll;
+    scroll-behavior: smooth;
+    //background-color: green;
+    padding-top: 10px;  //spacing on top of books imgs
+
+    &::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    /* Track */
+    &::-webkit-scrollbar-track {
+        background: #d6d6d6;
+        border-radius: 10px;
+    }
+
+    /* Handle */
+    &::-webkit-scrollbar-thumb {
+        background: #5397AC;
+        border-radius: 10px;
+    }
+
+    /* Handle on hover */
+    &::-webkit-scrollbar-thumb:hover {
+        background: #555;
+    }
+`;
+
+const ContentBox = styled.div`
+    width: 2250px;
+    display: flex;
+    align-items: center;
+    gap: 15px;          //spacing between cards
+    //background-color: blue;
+`;
+
+const CPcard = styled.div`
+    width: 150px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: ${(props) => props.color};
+
+    &:hover {
+        transform: scale(1.1); /* Increase the size of the card on hover */
+        transition: transform 0.3s ease; /* Add a smooth transition effect */
+    }
+`;
+
+const BookCover = styled.img`
+    width: 150px;
+    height: 200px;
+
+    /* hover effect */
+    ${CPcard}:hover & {
+        filter: brightness(70%); 
+    }
+`;
+
+const BookTitle = styled.p`
+    font-size: 16px;
+    margin-top: 10px;
+`;
--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -1,105 +1,70 @@
-import React, { useRef, useState } from 'react';
-import quizRec01 from "../assets/book1.png";
-import quizRec02 from "../assets/book2.png";
-import quizRec03 from "../assets/book3.png";
-import quizRec04 from "../assets/book4.png";
-import quizRec05 from "../assets/book5.png";
-import quizRec06 from "../assets/book6.png";
-import quizRec07 from "../assets/book7.png";
-import quizRec08 from "../assets/book8.png";
-import quizRec09 from "../assets/book9.png";
-import quizRec10 from "../assets/book10.png";
-// import rightArrow from "../assets/RightArrow.png";
-// import leftArrow from "../assets/LeftArrow.png";
+import React, { useRef } from 'react';
+import quizRec01 from "../assets/QuizRecBooks/book1.png";
+import quizRec02 from "../assets/QuizRecBooks/book2.png";
+import quizRec03 from "../assets/QuizRecBooks/book3.png";
+import quizRec04 from "../assets/QuizRecBooks/book4.png";
+import quizRec05 from "../assets/QuizRecBooks/book5.png";
+import quizRec06 from "../assets/QuizRecBooks/book6.png";
+import quizRec07 from "../assets/QuizRecBooks/book7.png";
+import quizRec08 from "../assets/QuizRecBooks/book8.png";
+import quizRec09 from "../assets/QuizRecBooks/book9.png";
+import quizRec10 from "../assets/QuizRecBooks/book10.png";
 import styled from 'styled-components';
 
-// const ITEM_WIDTH = 214;
 
-const CPPicks = () => {
-    // const [scrollPosition, setScrollPosition] = useState(0);
+const QuizRecs = () => {
     const containerRef = useRef();
 
     // Data array
     const QUIZ_DATA = [
-        { id: "01", image: quizRec01, details: "Tess of the Road by Rachel Hartman" },
-        { id: "02", image: quizRec02, details: "The First Bright Thing by J.R. Dawson" },
-        { id: "03", image: quizRec03, details: "A Tempest of Tea by Hafsah Faizal"},
-        { id: "04", image: quizRec04, details: "Sword Catcher by Cassandra Clare" },
-        { id: "05", image: quizRec05, details: "Supernova by Marissa Meyer" },
-        { id: "06", image: quizRec06, details: "Immortal Longings by Chloe Gong" },
-        { id: "07", image: quizRec07, details: "Stone Blind by Natalie Haynes" },
-        { id: "08", image: quizRec08, details: "Percy Jackson by Rick Riordan" },
-        { id: "09", image: quizRec09, details: "Harry Potter and the Goblet of Fire by J.K. Rowling" },
-        { id: "10", image: quizRec10, details: "The Martian by Andy Weir" },
+        { id: "01", cover: quizRec01, details: "Tess of the Road by Rachel Hartman" },
+        { id: "02", cover: quizRec02, details: "The First Bright Thing by J.R. Dawson" },
+        { id: "03", cover: quizRec03, details: "A Tempest of Tea by Hafsah Faizal"},
+        { id: "04", cover: quizRec04, details: "Sword Catcher by Cassandra Clare" },
+        { id: "05", cover: quizRec05, details: "Supernova by Marissa Meyer" },
+        { id: "06", cover: quizRec06, details: "Immortal Longings by Chloe Gong" },
+        { id: "07", cover: quizRec07, details: "Stone Blind by Natalie Haynes" },
+        { id: "08", cover: quizRec08, details: "Percy Jackson by Rick Riordan" },
+        { id: "09", cover: quizRec09, details: "Harry Potter and the Goblet of Fire by J.K. Rowling" },
+        { id: "10", cover: quizRec10, details: "The Martian by Andy Weir" },
     ];
 
-    // Function to handle scroll when button clicked
-    // const handleScroll = (scrollAmount) => {
-    //     // Calculate new scroll position
-    //     const newScrollPosition = scrollPosition + scrollAmount;
-
-    //     // Update new scroll position
-    //     setScrollPosition(newScrollPosition);
-
-    //     // Set scrollLeft
-    //     containerRef.current.scrollLeft = newScrollPosition;
-    // };
-
     return (
-        <Container>
+        <QuizRecContainer>
             <ScrollContainer ref={containerRef}>
-                <ContentBox>
-                    {QUIZ_DATA.map((item) => (
-                        <BookCard 
-                            key={item.id} 
-                            image={item.image}
-                            details={item.details}
-                        />
+                <QuizContentBox>
+                    {QUIZ_DATA.map((QuizRecData) => (
+                        <QRCard key={QuizRecData.id}>
+                            <img src={QuizRecData.cover} alt={QuizRecData.details} />
+                            
+                            <HoverOverlay>{QuizRecData.details}</HoverOverlay>
+                        
+                        </QRCard>
                     ))}
-                </ContentBox>
+                </QuizContentBox>
             </ScrollContainer>
- 
-            {/* <ActionButtons> */}
-                {/*Code before*/}
-                {/* <Button onClick={() => { handleScroll(-ITEM_WIDTH) }}>Scroll Left</Button> */}
-                
-                {/* <Button
-                    style={{ backgroundImage: `url(${leftArrow})` }}
-                    onClick={() => { handleScroll(-ITEM_WIDTH) }}
-                    >
-                </Button>
-               
-                <Button
-                    style={{ backgroundImage: `url(${rightArrow})` }}
-                    onClick={() => { handleScroll(ITEM_WIDTH) }}
-                    >
-                </Button> */}
-
-            {/* </ActionButtons> */}
-        </Container>
+        </QuizRecContainer>
     );
 };
+export default QuizRecs;
 
-export default CPPicks;
 
 // Styled components
-const Container = styled.div`
-    width: 100vw;
-    height: 60vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+const QuizRecContainer = styled.div`
+    //override by scroll container
 `;
 
 const ScrollContainer = styled.div`
-    width: 1150px;
-    height: 275px; //moves scroll bar
+    width: 1500px; //1150px;
+    height: 315px; //moves scroll bar
+    display: flex;
+    flex-direction: row;
+
     overflow-x: scroll;
     scroll-behavior: smooth;
-    padding-top: 20px;
-    padding-left: 10px;
-    padding-right: 10px;
+    padding: 10px;
+
+    //background-color: green;
 
     &::-webkit-scrollbar {
         width: 10px;
@@ -123,89 +88,212 @@ const ScrollContainer = styled.div`
     }
 `;
 
-const ContentBox = styled.div`
-    width: 2250px;
+const QuizContentBox = styled.div`
+    //width: 2250px;
     display: flex;
     align-items: center;
     gap: 20px;
+
+    //background-color: blue;
 `;
 
-const BookCard = ({ image, details }) => {
-    const [isHovered, setIsHovered] = useState(false);
-
-    return (
-        <StyledBookCard 
-            image={image}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-        >
-            {isHovered && 
-                <DetailsContainer>
-                    <Details>{details}</Details>
-                </DetailsContainer>  
-            }
-
-        </StyledBookCard>
-    );
-};
-
-const StyledBookCard = styled.div`
-    width: 145px; //214
-    height: 214px;  //314
+const QRCard = styled.div`
     display: flex;
-    align-items: center;
-    justify-content: center;
-    background-image: ${(props) => `url(${props.image})`};
-    background-size: contain;
+    margin: 10px;
+    width: 145px;
+    height: 214px;
+    position: relative;
 
+    img {
+        width: auto; 
+        height: 100%;
+        transition: transform 0.2s ease;
+    }
 
-    transition: transform 0.2s ease;
-    &:hover {
+    &:hover img {
         transform: scale(1.1);
     }
 `;
 
-const DetailsContainer = styled.div`
+const HoverOverlay = styled.div`
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    
+    background-color: rgba(0, 0, 0, 0.3);
+    color: white;
     display: flex;
     justify-content: center;
     align-items: center;
+    opacity: 0;
+    
+    transition: opacity 0.3s ease;
+
+    ${QRCard}:hover & {
+        opacity: 1;
+    }
 `;
 
-const Details = styled.p`
-    color: white;
-    font-size: 20px;
-    font-weight: bold;
-    text-align: center;
-`;
 
 
 
-// const ActionButtons = styled.div`
+//// OLD CODE
+// import React, { useRef, useState } from 'react';
+// import quizRec01 from "../assets/book1.png";
+// import quizRec02 from "../assets/book2.png";
+// import quizRec03 from "../assets/book3.png";
+// import quizRec04 from "../assets/book4.png";
+// import quizRec05 from "../assets/book5.png";
+// import quizRec06 from "../assets/book6.png";
+// import quizRec07 from "../assets/book7.png";
+// import quizRec08 from "../assets/book8.png";
+// import quizRec09 from "../assets/book9.png";
+// import quizRec10 from "../assets/book10.png";
+// import styled from 'styled-components';
+
+
+// const CPPicks = () => {
+//     const containerRef = useRef();
+
+//     // Data array
+//     const QUIZ_DATA = [
+//         { id: "01", image: quizRec01, details: "Tess of the Road by Rachel Hartman" },
+//         { id: "02", image: quizRec02, details: "The First Bright Thing by J.R. Dawson" },
+//         { id: "03", image: quizRec03, details: "A Tempest of Tea by Hafsah Faizal"},
+//         { id: "04", image: quizRec04, details: "Sword Catcher by Cassandra Clare" },
+//         { id: "05", image: quizRec05, details: "Supernova by Marissa Meyer" },
+//         { id: "06", image: quizRec06, details: "Immortal Longings by Chloe Gong" },
+//         { id: "07", image: quizRec07, details: "Stone Blind by Natalie Haynes" },
+//         { id: "08", image: quizRec08, details: "Percy Jackson by Rick Riordan" },
+//         { id: "09", image: quizRec09, details: "Harry Potter and the Goblet of Fire by J.K. Rowling" },
+//         { id: "10", image: quizRec10, details: "The Martian by Andy Weir" },
+//     ];
+
+//     return (
+//         <QuizRecContainer>
+//             <ScrollContainer ref={containerRef}>
+//                 <ContentBox>
+//                     {QUIZ_DATA.map((item) => (
+//                         <BookCard 
+//                             key={item.id} 
+//                             image={item.image}
+//                             details={item.details}
+//                         />
+//                     ))}
+//                 </ContentBox>
+//             </ScrollContainer>
+ 
+//         </QuizRecContainer>
+//     );
+// };
+
+// export default CPPicks;
+
+// // Styled components
+// const QuizRecContainer = styled.div`
+//     width: 100vw;
+//     height: 60vh;
 //     display: flex;
+//     flex-direction: column;
 //     align-items: center;
-//     gap: 1100px;
-//     margin-top: 20px;
+//     justify-content: center;
+
+//     background-color: blue;
 // `;
 
-// const Button = styled.button`
-//     width: 25px; /* Adjust width according to your arrow image */
-//     height: 50px; /* Adjust height according to your arrow image */
-//     background-color: transparent;
-//     border: none;
-//     cursor: pointer;
-//     background-size: cover;
-//     background-repeat: no-repeat;
-//     transition: all 0.5s ease;
+// const ScrollContainer = styled.div`
+//     width: 1150px;
+//     height: 275px; //moves scroll bar
+//     overflow-x: scroll;
+//     scroll-behavior: smooth;
+//     padding-top: 20px;
+//     padding-left: 10px;
+//     padding-right: 10px;
 
-//     &:hover {
-//         // Adjust styles for hover state if needed
+//     &::-webkit-scrollbar {
+//         width: 10px;
+//     }
+
+//     /* Track */
+//     &::-webkit-scrollbar-track {
+//         background: #d6d6d6;
+//         border-radius: 10px;
+//     }
+
+//     /* Handle */
+//     &::-webkit-scrollbar-thumb {
+//         background: #5397AC;
+//         border-radius: 10px;
+//     }
+
+//     /* Handle on hover */
+//     &::-webkit-scrollbar-thumb:hover {
+//         background: #555;
 //     }
 // `;
+
+// const ContentBox = styled.div`
+//     width: 2250px;
+//     display: flex;
+//     align-items: center;
+//     gap: 20px;
+// `;
+
+// const BookCard = ({ image, details }) => {
+//     const [isHovered, setIsHovered] = useState(false);
+
+//     return (
+//         <StyledBookCard 
+//             image={image}
+//             onMouseEnter={() => setIsHovered(true)}
+//             onMouseLeave={() => setIsHovered(false)}
+//         >
+//             {isHovered && 
+//                 <DetailsContainer>
+//                     <Details>{details}</Details>
+//                 </DetailsContainer>  
+//             }
+
+//         </StyledBookCard>
+//     );
+// };
+
+// const StyledBookCard = styled.div`
+//     width: 145px; //214
+//     height: 214px;  //314
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     background-image: ${(props) => `url(${props.image})`};
+//     background-size: contain;
+
+
+//     transition: transform 0.2s ease;
+//     &:hover {
+//         transform: scale(1.1);
+//     }
+// `;
+
+// const DetailsContainer = styled.div`
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     background-color: rgba(0, 0, 0, 0.5);
+    
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+// `;
+
+// const Details = styled.p`
+//     color: white;
+//     font-size: 20px;
+//     font-weight: bold;
+//     text-align: center;
+// `;
+
+
 
