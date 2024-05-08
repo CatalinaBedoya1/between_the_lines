7e8612cc0acf3fb68eb7c4622abb0c0e8c476066
MadelineLoo <madwasnthere@gmail.com>
2024-05-08 01:48:19 -0400

    spline community, audiobook

--- src/components/FixedBOTMButton.js
+++ src/components/FixedBOTMButton.js
@@ -2,13 +2,7 @@ import React, { useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 import BOTMText from '../assets/BOTMButton/BOTMText.png';
 import BOTMBookIcon from '../assets/BOTMButton/BOTMBookIcon.png';
-
-
-
-
 import Popup from './Popup';
-
-
 
 
 const rotate = keyframes`
@@ -16,36 +10,24 @@ const rotate = keyframes`
     to { transform: rotate(360deg); }
 `;
 
-
 const BOTMButton = () => {
-
-
     const [buttonPopup, setButtonPopup] = useState(false);
-
 
   return (
     <div>
     <BOTMbuttonContainer onClick={() => setButtonPopup(true)}>
-   
         <BOTMCircleText>
         <img src={BOTMText} alt="BOTMtext"/>
         </BOTMCircleText>
 
-
         <BOTMIcon>
             <img src={BOTMBookIcon} alt="BOTMbookicon"/>
         </BOTMIcon>
-
-
     </BOTMbuttonContainer>
-
 
     <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
 
-
     </Popup>
-
-
     </div>
  
   );
@@ -61,8 +43,7 @@ const BOTMbuttonContainer = styled.div`
     position: fixed;
     bottom: 16px;
     right: 16px;
-    z-index: 5;
-
+    z-index:5;
 
     //background-color:green;
 `;
@@ -73,6 +54,8 @@ const BOTMCircleText =styled.div`
     line-height: 0px;        //vertically
     position: relative;
     animation: ${rotate} 20s linear infinite;
+
+    z-index:5;
 `;
 
 
@@ -80,4 +63,6 @@ const BOTMIcon =styled.div`
     position: absolute;
     top:27px;
     left:27px;
+
+    z-index:5;
 `;
--- src/routes/Audiobooks.js
+++ src/routes/Audiobooks.js
@@ -1,31 +1,100 @@
 import React from 'react';
 import styled from 'styled-components';
-
-
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const audiobooks = () => {
     return (
+        <AudiobookContainer>
         <AudiobookHeader>
-           
+            <AudioHeaderText>Discover our audiobooks</AudioHeaderText>
+            
+            <AudioSearchBarContainer>
+                <AudioInput type="text" id="searchInput" placeholder="Find an audiobook ..." />
+                <SearchIcon>
+                    <FontAwesomeIcon icon={faSearch} />
+                </SearchIcon>
+            </AudioSearchBarContainer>
          
         </AudiobookHeader>
-   
+
+        <NewReleases>
+
+        </NewReleases>
+
+        {/* insert BOTM */}
+
+        <TrendingAudio>
+        </TrendingAudio>
+
+        </AudiobookContainer>
     );
 };
-
-
-
 
 export default audiobooks;
 
 
+const AudiobookContainer=styled.div`
+    display:flex;
+    flex-direction: column;
+    width:100wh;
+    height:auto;
+`;
+
 const AudiobookHeader =styled.div`
     position: relative;
     width: 100%;
-    height: 500px;
-    // padding-top: 100px;
-    // padding-left: 50px;
+    height: 800px;
    
     background-color: #3D326F;
+`;
+
+const AudioHeaderText=styled.div`
+    color: white;
+    font-size: 50px;
+    text-align: center;
+
+    padding-top:150px;
+`;
+
+
+const AudioSearchBarContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    width: 500px;
+`;
+
+const AudioInput = styled.input`
+    width: 500px;
+    border: 1px solid #ccc;
+    padding: 10px 50px ;
+    border-radius: 30px;
+    height: 50px;
+    font-color: #3F3C3C;
+    font-family: Manrope;
+`;
+
+const SearchIcon = styled.div`
+    position: absolute;
+    left: 10px;
+    color: #3F3C3C;
+`;
+
+
+const NewReleases =styled.div`
+    width: 100%;
+    height: 500px;
+
+    background-color: blue;
+`;
+
+const TrendingAudio=styled.div`
+    width: 100%;
+    height: 800px;
+
+    background-color: green;
 `;
